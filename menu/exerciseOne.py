6df0c9b07951b5c5dab05957f29fdf8e70877acf
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-19 21:21:21 -0500

    tuplas listas y diccionarios

--- menu/exerciseOne.py
+++ menu/exerciseOne.py
@@ -1,7 +1,11 @@
 from logic.exerciseOneListas import saveCourse
 
 def design():
-    course=input("What is the course name? ")
-    rta= saveCourse(course)
-    
-    print(rta)  
+    while True:
+        course=input("What is the course name? ")
+        rta= saveCourse(course)
+        
+        print(rta)  
+        desicion=int(input("You want continue? Y/1 N/0"))
+        if desicion==0:
+            break
---
+++ menu/menuListas.py
@@ -0,0 +1,9 @@
+def menuPrincipal():
+    print(f"""
+        TIPO DE EJERCICIOS
+    --------------------------
+    1. EJERCICIOS LISTAS Y TUPLAS
+    2. EJERCICIOS DICCIONARIOS     
+""")
+    desicion=int(input("Ingresa una opcion 1/2"))
+    return desicion
